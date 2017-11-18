09ca9896765d71c0f9d4c95150f72f7e95034321
Amit Prajapati <amit19851207@gmail.com>
2017-11-18 16:47:04 +0545

    hbl.php

---
+++ admin/language/english/payment/hbl.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']					= 'Himalayan Bank Limited';
+
+// Text
+$_['text_payment']					= 'Payment';
+$_['text_success']					= 'Success: You have modified HBL Payment payment module!';
+$_['text_edit']                     = 'Edit HBL Payment';
+
+// Entry
+$_['merchant_id'] 					= 'Merchant Id';
+$_['merchant_key'] 					= 'Merchant Key';
+$_['transaction_mode'] 				= 'Transaction Mode';
+$_['entry_total']					= 'Total';
+$_['entry_order_status']			= 'Order Status';
+$_['entry_geo_zone']				= 'Geo Zone';
+$_['entry_status']					= 'Status';
+$_['entry_sort_order']				= 'Sort Order';
+
+$_['error_merchantid'] = 'Merchant Id Required !';
+$_['error_merchantkey'] = 'Merchant Key Required !';
+
+// Help
+$_['help_total']					= 'The checkout total the order must reach before this payment method becomes active.';
+
+// Error
+$_['error_permission']				= 'Warning: You do not have permission to modify payment HBL Payment!';
